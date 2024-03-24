9cd7662a13f64f361b3e84774bd983a317ba1e0d
Pedro Gonzalez <pedro.gch01@gmail.com>
2024-03-24 02:41:26 -0300

    agrego js y modifico html

---
+++ mian.js
@@ -0,0 +1,10 @@
+let numero1 = 14
+let numero = 8
+
+if (numero1 > numero){
+  document.querySelector('h1').innerText ='el numero 1 es mas grande'
+}else{
+  
+}
+
+console.log('hola mundo mundillo')
